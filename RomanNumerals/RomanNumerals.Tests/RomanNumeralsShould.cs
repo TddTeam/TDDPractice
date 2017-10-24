ec030989b58bfc5ba6e61560dde169326e448a3b
kmitzu <camelia.hofnar@yahoo.com>
2017-10-24 17:58:56 +0100

    Added a test website

--- RomanNumerals/RomanNumerals.Tests/RomanNumeralsShould.cs
+++ RomanNumerals/RomanNumerals.Tests/RomanNumeralsShould.cs
@@ -21,10 +21,7 @@ namespace RomanNumerals.Tests
         [TestCase(11, "XI")]
         [TestCase(12, "XII")]
         [TestCase(13, "XIII")]
-        [TestCase(15, "XV")]
-        [TestCase(20, "XX")]
-        [TestCase(40, "XL")]
-        [TestCase(400, "CD")]
+        [TestCase(14, "XIV")]
         public void ReturnRomanNumeralForNumber(int number, string numeral)
         {
             var romanNumeralsConvertor = new RomanNumeralsConvertor();
@@ -40,7 +37,7 @@ public class RomanNumeralsConvertor
 {
     public string Convert(int number)
     {
-        var numerals = new Dictionary<int, char> { {1000, 'M'}, {500, 'D'}, {100, 'C'}, {50, 'L'}, {10, 'X'}, {5, 'V'}, {1, 'I'}};
+        var numerals = new Dictionary<int, char> { {10, 'X'}, {5, 'V'}, {1, 'I'}};
 
         string romanNumeral = "";
 
@@ -57,18 +54,9 @@ public class RomanNumeralsConvertor
                     break;
                 }
 
-                if (i == numeral.Key - 100)
+                if (i == numerals[10] )
                 {
-                    romanNumeral = numerals[100].ToString() + numeral.Value;
-
-                    i = 0;
-
-                    break;
-                }
-
-                if (i == numeral.Key - 10)
-                {
-                    romanNumeral = numerals[10].ToString() + numeral.Value;
+                    romanNumeral = "IV";
 
                     i = 0;
 
